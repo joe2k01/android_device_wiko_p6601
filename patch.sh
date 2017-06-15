be1993dc6f64f6e5d207f34ee7b7f76d822fa930
Zhaofeng Li <hello@zhaofeng.li>
2017-06-14 17:59:58 -0700

    patch.sh: Something fancy
    Change-Id: Ie54d15ea2c352a9ce62531c657ae18db5df0655d

--- patch.sh
+++ patch.sh
@@ -3,8 +3,19 @@ COMMAND=$(basename $0)
 TREE=$(realpath ./../../../)
 DEVICE=$(realpath ./)
 PATCHES=$DEVICE/patches
+PROJECTS=(
+	frameworks/av
+	system/core
+	bionic
+)
 
-PATCH_ARGS='-p1 '
+BOLD=$(tput bold)
+NORM=$(tput sgr0)
+WARN=$(tput setaf 1)
+SUCC=$(tput setaf 2)
+
+reverse=0
+failed=0
 case $1 in
 	"-h" | "--help")
 		echo "Usage: $COMMAND [--reverse]"
@@ -14,25 +25,75 @@ case $1 in
 		;;
 	"-R" | "--reverse")
 		echo "Reversing patches..."
-		PATCH_ARGS+='-R'
+		reverse=1
 		;;
 	*)
 		echo "Applying patches..."
 		;;
 esac
 
-pushd $TREE/system/core
-patch $PATCH_ARGS < $PATCHES/system_core/remove-CAP_SYS_NICE-from-surfaceflinger-and-cln-logger-service.patch
-patch $PATCH_ARGS < $PATCHES/system_core/Add-fixes-for-MTK-devices.patch
-popd
+for project in "${PROJECTS[@]}"
+do
+	echo "${BOLD}Entering $project${NORM}"
+	pushd $TREE/$project > /dev/null
 
-pushd $TREE/bionic
-patch $PATCH_ARGS < $PATCHES/bionic/Temporary-apply-LIBC-version-to-__pthread_gettid.patch
-popd
+	patchdir=`echo $project | sed -e 's/\//_/g'`
+	for patch in $PATCHES/$patchdir/*.patch
+	do
+		patchname=`basename "$patch"`
+		echo -n "$patchname: "
 
-pushd $TREE/frameworks/av
-patch $PATCH_ARGS < $PATCHES/frameworks_av/mtk-audio-fix.patch
-patch $PATCH_ARGS < $PATCHES/frameworks_av/Disable-usage-of-get_capture_position.patch
-patch $PATCH_ARGS < $PATCHES/frameworks_av/fix-access-wvm-to-ReadOptions.patch
-patch $PATCH_ARGS < $PATCHES/frameworks_av/Add-missing-MediaBufferGroup__acquire_buffer_symbols.patch
-popd
+		if [ "$reverse" = 1 ]
+		then
+			# Is this patch NOT applied?
+			patch -p1 -N --dry-run < $patch > /dev/null 2>&1
+			if [ $? -eq 0 ]
+			then
+				echo "${SUCC}Not applied${NORM}"
+				continue
+			else
+				# Can this patch be cleanly reversed?
+				patch -p1 -NR --dry-run --silent < $patch 2> /dev/null
+				if [ $? -eq 0 ]
+				then
+					patch -p1 -NR < $patch
+					echo "${SUCC}Reversed${NORM}"
+				else
+					echo "${WARN}Impossible to reverse patch cleanly. Skipping...${NORM}"
+					failed=1
+				fi
+			fi
+		else
+			# Has this patch already been applied?
+			patch -p1 -NR --dry-run < $patch > /dev/null 2>&1
+			if [ $? -eq 0 ]
+			then
+				echo "${SUCC}Already applied${NORM}"
+				continue
+			else
+				# Can this patch be cleanly applied?
+				patch -p1 -N --dry-run --silent < $patch 2>/dev/null
+				if [ $? -eq 0 ]
+				then
+					patch -p1 -N < $patch
+					echo "${SUCC}Applied${NORM}"
+				else
+					echo "${WARN}Impossible to apply patch cleanly. Skipping...${NORM}"
+					failed=1
+				fi
+			fi
+		fi
+	done
+
+	echo "${BOLD}Leaving $project${NORM}"
+	echo
+	popd > /dev/null
+done
+
+if [ $failed = 1 ]
+then
+	echo "${WARN}One or more patches failed.${NORM}"
+	exit 1
+else
+	echo "${SUCC}All good.${NORM}"
+fi
